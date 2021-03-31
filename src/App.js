39bb4b98755591e5dc01e8e98910e4d13c9f78ce
BRvanHogen <brvanhogen@hotmail.nl>
2021-03-31 20:25:10 +0200

    got one fact displayed on the page, drafted component Cat.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
 import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import Cat from "./components/Cat";
+
+
 
 function App() {
-  return (
-    <h1>maak hier de app</h1>
-  );
+    const [catFact, setCatFact] = useState([]);
+
+    async function throwCatFacts() {
+        try {
+            const response = await axios.get(`https://cat-fact.herokuapp.com/facts`);
+            console.log(response.data);
+            setCatFact(response.data[0].text);
+            console.log(catFact);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    //function call
+    useEffect(() => {
+        throwCatFacts();
+    }, []);
+
+
+    return (
+        <>
+            <h1>First Cat App Ever</h1>
+            <p>{catFact}</p>
+        </>
+        );
+
 }
 
 export default App;
+
+
+// nu moet ik alle vijf object-onderdelen uit de array zien te implementeren op de pagina.
