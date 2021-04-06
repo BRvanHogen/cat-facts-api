9a9af5d0e5802f18bcf464346d28ed10f22a4cff
BRvanHogen <brvanhogen@hotmail.nl>
2021-04-06 18:40:05 +0200

    Added variable to response Array, implemented buttons in App.js to scroll through cat facts.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import Cat from "./components/Cat";
 
 function App() {
     const [catFact, setCatFact] = useState([]);
+    const [number, setNumber] = useState(0);
 
     async function throwCatFacts() {
         try {
             const response = await axios.get(`https://cat-fact.herokuapp.com/facts`);
             console.log(response.data);
-            setCatFact(response.data[0].text);
+            setCatFact(response.data[number].text);
             console.log(catFact);
         } catch (e) {
             console.error(e);
@@ -22,19 +23,31 @@ function App() {
     //function call
     useEffect(() => {
         throwCatFacts();
-    }, []);
+    }, [number]);
 
 
     return (
         <>
             <h1>First Cat App Ever</h1>
             <p>{catFact}</p>
+            <button
+                type="button"
+                disabled={number === 0}
+                onClick={()=> {setNumber(number-1)}}
+            >
+                purr-evious fact
+            </button>
+            <button
+                type="button"
+                disabled={number === 4}
+                onClick={()=>  {setNumber(number+1)}}
+            >
+                next fact
+            </button>
         </>
         );
-
 }
 
+
+
 export default App;
-
-
-// nu moet ik alle vijf object-onderdelen uit de array zien te implementeren op de pagina.
